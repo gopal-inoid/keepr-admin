112b5e10f3fcade284c7235d47dbb49725ce60ba
Amaan <amaan.khan@technosoftwares.co.in>
2023-03-01 19:30:03 +0530

    initial commit

---
+++ resources/views/layouts/back-end/partials/_footer.blade.php
@@ -0,0 +1,42 @@
+
+
+<div class="footer">
+    <div class="row justify-content-between align-items-center">
+        <div class="col-lg-4 mb-3 mb-lg-0">
+            <p class="font-size-sm mb-0 title-color text-center text-lg-left">
+                &copy; iNoid Solutions All Right Reserve <?php echo date('Y'); ?>
+               {{-- &copy; 
+                {{\App\Model\BusinessSetting::where(['type'=>'company_name'])->first()->value}}. 
+                <span class="d-none d-sm-inline-block">{{\App\Model\BusinessSetting::where(['type'=>'company_copyright_text'])->first()->value}}</span> --}}
+            </p>
+        </div>
+        <div class="col-lg-8">
+            <div class="d-flex justify-content-center justify-content-lg-end">
+                <!-- List Dot -->
+                <ul class="list-inline list-footer-icon justify-content-center justify-content-lg-start mb-0">
+                    <li class="list-inline-item">
+                        <a class="list-separator-link" href="{{route('admin.business-settings.web-config.index')}}">
+                            <i class="tio-settings"></i>
+                            {{\App\CPU\translate('Business Setup')}}
+                        </a>
+                    </li>
+
+                    <li class="list-inline-item">
+                        <a class="list-separator-link"href="{{route('admin.profile.update',auth('admin')->user()->id)}}">
+                            <i class="tio-user"></i>
+                            {{\App\CPU\translate('Profile')}}
+                        </a>
+                    </li>
+
+                    <li class="list-inline-item">
+                        <a class="list-separator-link" href="{{route('admin.dashboard.index')}}">
+                            <i class="tio-home-outlined"></i>
+                            {{\App\CPU\translate('Home')}}
+                        </a>
+                    </li>
+                </ul>
+                <!-- End List Dot -->
+            </div>
+        </div>
+    </div>
+</div>
