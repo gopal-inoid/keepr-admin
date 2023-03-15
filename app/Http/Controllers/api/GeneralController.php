4138133c4828db73144f32cd884c470592dfff9c
Amaan <amaan.khan@technosoftwares.co.in>
2023-03-15 20:05:43 +0530

    attempt to http bug fixes

--- app/Http/Controllers/api/GeneralController.php
+++ app/Http/Controllers/api/GeneralController.php
@@ -18,6 +18,8 @@ class GeneralController extends Controller
 {
     public function get_pages(Request $request){ // page type = terms_condition, privacy_policy, support, about_us
         echo ' <pre> ' . print_r($request->all()); die;
+        $query = $request->getQueryString();
+
         // $response = Http::get(env('APP_URL'));
         // echo "<pre>"; print_r($response->body()); die;
         if($request->page_name){
