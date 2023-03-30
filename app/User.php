7427ecc8432491c239ebdcab91d318c5652416d7
Amaan <amaan.khan@technosoftwares.co.in>
2023-03-30 11:00:01 +0530

    bug fixed in user auth

--- app/User.php
+++ app/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'f_name', 'l_name', 'name', 'email', 'password', 'phone', 'image', 'login_medium','is_active','social_id','is_phone_verified','temporary_token','firebase_auth_id','auth_access_token'
+        'f_name', 'l_name', 'name', 'email', 'password', 'phone', 'image', 'login_medium','is_active','social_id','is_phone_verified','temporary_token','firebase_auth_id','auth_access_token','phone_code'
     ];
 
     /**
