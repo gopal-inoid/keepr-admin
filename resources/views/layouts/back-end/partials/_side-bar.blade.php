39ac6f1d19e97fbc58ee64f2db7d32cdc9cfabf2
Amaan <amaan.khan@technosoftwares.co.in>
2023-05-08 12:28:15 +0530

    countries and states APIs

--- resources/views/layouts/back-end/partials/_side-bar.blade.php
+++ resources/views/layouts/back-end/partials/_side-bar.blade.php
@@ -145,6 +145,8 @@
                                 <i class="tio-shopping nav-icon"></i>
                                 <span class="navbar-vertical-aside-mini-mode-hidden-elements text-truncate">Shipping Methods</span>
                             </a>
+                        </li>
+                        <li class="navbar-vertical-aside-has-menu {{ Request::is('admin/business-settings/shipping-method/tax-calculation') ?'active':''}}">
                             <a class="js-navbar-vertical-aside-menu-link nav-link"
                                href="{{route('admin.business-settings.shipping-method.tax-calculation')}}" title="Shipping Methods">
                                 <i class="tio-shopping nav-icon"></i>
