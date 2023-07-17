b80fcdbb4997d13944835146579bf872020b6ca1
Amaan <amaan.khan@technosoftwares.co.in>
2023-07-17 17:10:39 +0530

    fixed bug from product detail API

--- app/Http/Controllers/api/CartController.php
+++ app/Http/Controllers/api/CartController.php
@@ -426,28 +426,38 @@ class CartController extends Controller
 
     public function CreateCheckout($amount){
 
-        Stripe::setApiKey('sk_test_51MprMPC6n3N1q7nDsYGlAYsLmkhVVQ2LAQqbInlthpU9FoUdqsNy9jT8uhMRrg1e6KtptrHJhY5iwJc3ASXxALeg005ync97Mg');
-        header('Content-Type: application/json');
-        $YOUR_DOMAIN = url('/');
-        $checkout_session = \Stripe\Checkout\Session::create([
-            'payment_method_types' => ['card'],
-            'line_items' => [[
-                'price_data' => [
-                    'currency' => 'USD',
-                    'unit_amount' => round($amount, 2) * 100,
-                    'product_data' => [
-                        'name' => 'Keepr',
-                        'images' => [asset('storage/app/public/company') . '/' . Helpers::get_business_settings('company_web_logo')],
-                    ],
-                ],
-                'quantity' => 1,
-            ]],
-            'mode' => 'payment',
-            'success_url' =>  $YOUR_DOMAIN . '/pay-stripe/success',
-            'cancel_url' => url()->previous(),
+        //$amount = 2000;
+        //Stripe::setApiKey('sk_test_51MprMPC6n3N1q7nDsYGlAYsLmkhVVQ2LAQqbInlthpU9FoUdqsNy9jT8uhMRrg1e6KtptrHJhY5iwJc3ASXxALeg005ync97Mg');
+        //header('Content-Type: application/json');
+        //$YOUR_DOMAIN = url('/');
+        // $checkout_session = \Stripe\Checkout\Session::create([
+        //     'payment_method_types' => ['card'],
+        //     'line_items' => [[
+        //         'price_data' => [
+        //             'currency' => 'USD',
+        //             'unit_amount' => round($amount, 2) * 100,
+        //             'product_data' => [
+        //                 'name' => 'Keepr',
+        //                 'images' => [asset('storage/app/public/company') . '/' . Helpers::get_business_settings('company_web_logo')],
+        //             ],
+        //         ],
+        //         'quantity' => 1,
+        //     ]],
+        //     'mode' => 'payment',
+        //     'success_url' =>  $YOUR_DOMAIN . '/pay-stripe/success',
+        //     'cancel_url' => url()->previous(),
+        // ]);
+
+        $stripe = new \Stripe\StripeClient('sk_test_51MprMPC6n3N1q7nDsYGlAYsLmkhVVQ2LAQqbInlthpU9FoUdqsNy9jT8uhMRrg1e6KtptrHJhY5iwJc3ASXxALeg005ync97Mg');
+        $paymentIntents = $stripe->paymentIntents->create([
+            'amount' => round($amount, 2) * 100,
+            'currency' => 'usd',
+            'automatic_payment_methods' => [
+                'enabled' => true,
+            ],
         ]);
 
-       return $checkout_session;
+       return $paymentIntents;
 
     }
     
