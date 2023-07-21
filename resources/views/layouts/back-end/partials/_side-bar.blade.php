f909c0391f4f3952ec40f5613bcb9d47d8fcf3b6
Amaan <amaan.khan@technosoftwares.co.in>
2023-07-21 13:18:39 +0530

    but fixes

--- resources/views/layouts/back-end/partials/_side-bar.blade.php
+++ resources/views/layouts/back-end/partials/_side-bar.blade.php
@@ -85,9 +85,9 @@
                             </li>
                             <li class="navbar-vertical-aside-has-menu {{ Request::is('admin/product/current-active-device')?'active':''}}">
                                 <a class="js-navbar-vertical-aside-menu-link nav-link"
-                                href="{{route('admin.product.current-active-device')}}" title="{{ \App\CPU\translate('Current Active Devices') }}">
+                                href="{{route('admin.product.current-active-device')}}" title="{{ \App\CPU\translate('Connected Devices') }}">
                                     <i class="tio-globe nav-icon"></i>
-                                    <span class="navbar-vertical-aside-mini-mode-hidden-elements text-truncate">{{ \App\CPU\translate('Current Active Devices') }}</span>
+                                    <span class="navbar-vertical-aside-mini-mode-hidden-elements text-truncate">{{ \App\CPU\translate('Connected Devices') }}</span>
                                 </a>
                             </li>
                         @endif
