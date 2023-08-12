c04dc998e6ee3361ed0ae5ec3e118d65a4faf689
Armaan Majeed Khan <armaan.inoidsolutions.com>
2023-08-12 15:25:51 +0530

    UI Changes

--- resources/views/keepr_app/index.blade.php
+++ resources/views/keepr_app/index.blade.php
@@ -6,8 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Keepr App</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('keepr_app_assets/style.css') }}">
     <link rel="icon" type="image/x-icon" href="{{asset("public/company/Keepr-logo-black.png")}}">
 </head>
@@ -20,14 +19,11 @@
                 <a class="navbar-brand" href="#">
                     <img src="{{ asset('keepr_app_assets/assests/Keepe_logo.png')}}" alt="">
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto m-auto my-2 my-lg-0 navbar-nav-scroll"
-                        style="--bs-scroll-height: 100px;">
+                    <ul class="navbar-nav me-auto m-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
                         <li class="nav-item">
                             <a class="nav-link" aria-current="page" href="#home">Home</a>
                         </li>
@@ -45,7 +41,7 @@
                         </li>
                     </ul>
                     <form class="d-flex m-3">
-                        <a class="btn download_keeper_btn" href="#downloads" type="submit">Download Keepr</a>
+                        <a class="btn download_keeper_btn" href="#downloads" type="button">Download Keepr</a>
                     </form>
                 </div>
             </div>
@@ -59,18 +55,18 @@
             <div class="For_picture">
                 <div class="container" id="Valuables_again">
                     <div class="row">
-                        <div class="leftKeeprPic col-xl-5 col-md-6" id="leftKeeprPic">
+                        <div class="leftKeeprPic col-xl-5 col-md-5" id="leftKeeprPic">
                             <div class="Main_Content">
                                 <span class="main_heading">Always keep your stuff with you</span>
                                 <p>Do you always lose or forget your stuff before you go out? Keeper can help with that
                                     by alerting you when you leave your things behind!</p>
                                 <div class="download_app_btn">
-                                    <a href="#downloads" class="btn" type="button ">Download Keepr</a>
+                                    <a href="#downloads" class="btn" type="button">Download Keepr</a>
                                 </div>
                             </div>
 
                         </div>
-                        <div class="col-xl-7 col-md-6">
+                        <div class="col-xl-7 col-md-7">
                             <div class="rightMobileIcon">
                                 <img src="{{ asset('keepr_app_assets/assests/KeeprDistance.png') }}" alt="">
 
@@ -87,7 +83,7 @@
             <div class="container">
                 <div class="About_seocond row">
                     <div class="col-md-6">
-                        <div class="rightMobileIcon2">
+                        <div class="rightMobileIcon2 d-flex align-items-center">
                             <img src="{{ asset('keepr_app_assets/assests/Keepr-about.png') }}" alt="">
                         </div>
                     </div>
@@ -202,8 +198,7 @@
         <div class="container-fluid pt-5">
             <div class="overlay">
                 <div class="overlay-image">
-                    <img src="{{ asset('keepr_app_assets/assests/PlayButtonIcon.png') }}" data-toggle="modal"
-                        data-target="#exampleModalCenter" class="video_Play_Button" alt="video_Play_Button">
+                    <img src="{{ asset('keepr_app_assets/assests/PlayButtonIcon.png') }}" data-toggle="modal" data-target="#exampleModalCenter" class="video_Play_Button" alt="video_Play_Button">
 
                 </div>
             </div>
@@ -216,7 +211,7 @@
                 <div class="row justify-content-evenly mt-5">
                     @if(!empty($Products))
                     @foreach($Products as $k => $val)
-                    <div class="col-md-12 col-lg-5 col-xl-5 Product-1 mx-1 my2">
+                    <div class="col-md-8 col-lg-5 col-xl-5 Product-1 mx-1 my2">
                         <img src="{{ asset('/product/thumbnail/'.$val["thumbnail"])}}" alt="">
                         <div>
                             <h4 class="ibeacon-headline">{{$val['name']}}</h4>
@@ -235,8 +230,7 @@
                     @endforeach
                     @endif
                 </div>
-                <div class="container mt-lg-5 pt-lg-5 mt-md-5 pt-md-5 Frequently_Questions" id="faq"
-                    style="padding-top: 6rem !important;">
+                <div class="container mt-lg-5 pt-lg-5 mt-md-5 pt-md-5 Frequently_Questions" id="faq" style="padding-top: 6rem !important;">
                     <h1 class="text-center" style="font-weight: 600">Frequently Asked Questions</h1>
                     <div class="row justify-content-center mt-5 pb-5">
                         @if(!empty($faqs))
@@ -292,8 +286,7 @@
                         </div> -->
                     </div>
                     <h4 class="text-center mt-5 mb-5">
-                        <b>Haven't find suitable answer? </b><b class="tellUs"><a
-                                href="mailto:support@thekeeprapp.com">Tell us what you need</a></b>
+                        <b>Haven't find suitable answer? </b><b class="tellUs"><a href="mailto:support@thekeeprapp.com">Tell us what you need</a></b>
                     </h4>
                 </div>
             </div>
@@ -312,8 +305,7 @@
                                             &nbsp; support@thekeeprapp.com</a>
                                     </div>
                                     <div class="mail_section" id="mail_section">
-                                        <img src="{{ asset('keepr_app_assets/assests/Mobile_Number_Icon.svg') }}"
-                                            alt="">
+                                        <img src="{{ asset('keepr_app_assets/assests/Mobile_Number_Icon.svg') }}" alt="">
                                         <a href="tel:2125551212"> &nbsp; +1
                                             647-614-1496</a>
 
@@ -337,8 +329,7 @@
                         <div class="row" style="position: relative;">
                             <div class="col-md-12 footer-body">
 
-                                <img src="{{ asset('keepr_app_assets/assests/Footer_Log.png') }}" class="footer-img"
-                                    alt="">
+                                <img src="{{ asset('keepr_app_assets/assests/Footer_Log.png') }}" class="footer-img" alt="">
 
                             </div>
                             <div class="footer-body2">
@@ -375,15 +366,11 @@
                                         </h1>
                                         <p class="mt-4">Leave us a rating and review!</p>
                                         <div class="app_download_btn">
-                                            <a href="https://play.google.com/store/apps/details?id=com.keepr.android"
-                                                target="_blank" class="m-md-1">
-                                                <img src="{{ asset('keepr_app_assets/assests/PlayStore_Icon.svg') }}"
-                                                    alt="">
+                                            <a href="https://play.google.com/store/apps/details?id=com.keepr.android" target="_blank" class="m-md-1">
+                                                <img src="{{ asset('keepr_app_assets/assests/PlayStore_Icon.svg') }}" alt="">
                                             </a>
-                                            <a href="https://apps.apple.com/us/app/keepr-app/id6449671498"
-                                                target="_blank" class="m-md-1">
-                                                <img src="{{ asset('keepr_app_assets/assests/AppStore_Icon.svg') }}"
-                                                    alt="">
+                                            <a href="https://apps.apple.com/us/app/keepr-app/id6449671498" target="_blank" class="m-md-1">
+                                                <img src="{{ asset('keepr_app_assets/assests/AppStore_Icon.svg') }}" alt="">
                                             </a>
                                         </div>
                                     </div>
@@ -400,12 +387,9 @@
                                 </div>
                                 <div class="CopyRight_Section">
 
-                                    <img class="mx-1" src="{{ asset('keepr_app_assets/assests/FaceBook_Icon.svg') }}"
-                                        alt="">
-                                    <img class="mx-1" src="{{ asset('keepr_app_assets/assests/TwitterIcon.svg') }}"
-                                        alt="">
-                                    <img class="mx-1" src="{{ asset('keepr_app_assets/assests/InstgramIcon.svg') }}"
-                                        alt="">
+                                    <img class="mx-1" src="{{ asset('keepr_app_assets/assests/FaceBook_Icon.svg') }}" alt="">
+                                    <img class="mx-1" src="{{ asset('keepr_app_assets/assests/TwitterIcon.svg') }}" alt="">
+                                    <img class="mx-1" src="{{ asset('keepr_app_assets/assests/InstgramIcon.svg') }}" alt="">
                                 </div>
                             </div>
                         </div>
@@ -417,8 +401,7 @@
 
 
         <!-- Modal -->
-        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog"
-            aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
@@ -428,49 +411,42 @@
                         </button>
                     </div>
                     <div class="modal-body">
-                        <iframe class="Youtube_Video" width="560" height="315"
-                            src="https://www.youtube.com/embed/D0UnqGm_miA" title="YouTube video player" frameborder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                            allowfullscreen></iframe>
+                        <iframe class="Youtube_Video" width="560" height="315" src="https://www.youtube.com/embed/D0UnqGm_miA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                     </div>
                 </div>
             </div>
         </div>
 </body>
 
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
 </script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
-    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
 </script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
-    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
 </script>
-<script src="https://code.jquery.com/jquery-3.6.4.min.js"
-    integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
 
 <script>
-$(document).ready(function() {
-    $(".desc-1").hover(function() {
-        $(this).addClass("active");
-        $(".desc-2").removeClass("active");
-        $(".desc-3").removeClass("active");
-        $(".desc-4").removeClass("active");
-    })
-    $(".desc-2").hover(function() {
-        $(this).addClass("active");
-        $(".desc-1").removeClass("active");
-        $(".desc-3").removeClass("active");
-        $(".desc-4").removeClass("active");
-    })
-    $(".desc-3").hover(function() {
-        $(this).addClass("active");
-        $(".desc-2").removeClass("active");
-        $(".desc-1").removeClass("active");
-        $(".desc-4").removeClass("active");
+    $(document).ready(function() {
+        $(".desc-1").hover(function() {
+            $(this).addClass("active");
+            $(".desc-2").removeClass("active");
+            $(".desc-3").removeClass("active");
+            $(".desc-4").removeClass("active");
+        })
+        $(".desc-2").hover(function() {
+            $(this).addClass("active");
+            $(".desc-1").removeClass("active");
+            $(".desc-3").removeClass("active");
+            $(".desc-4").removeClass("active");
+        })
+        $(".desc-3").hover(function() {
+            $(this).addClass("active");
+            $(".desc-2").removeClass("active");
+            $(".desc-1").removeClass("active");
+            $(".desc-4").removeClass("active");
+        });
     });
-});
 </script>
 
 </html>
