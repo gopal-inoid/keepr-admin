1c58837cc318b3cc5f42fb912f1745cc25c9092c
Amaan <amaan@inoidsolutions.com>
2023-08-21 12:45:48 +0530

    changes in connected device status

--- app/Http/Controllers/api/ProductController.php
+++ app/Http/Controllers/api/ProductController.php
@@ -352,7 +352,7 @@ class ProductController extends Controller
 													'minor' => $devices->minor,
 													'user_id'=>$user_details->id
 													])->first();
-                        $get_all_devices[$k]['device_request_status'] = $device_request->status ?? ''; // 0 = lost , 1 = found
+                        $get_all_devices[$k]['device_request_status'] = $device_request->status ?? 2; // 0 = lost , 1 = found // 2 means not sent request
                         if(!empty($device_info->thumbnail)){
                             $get_all_devices[$k]['thumbnail'] = asset("/product/thumbnail/$device_info->thumbnail");
                         }else{
