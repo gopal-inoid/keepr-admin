ee0b4305d4e58ae5f0819dbbc672037c8f0c8e38
Amaan <amaan@inoidsolutions.com>
2023-08-23 12:27:52 +0530

    cron setup for order status update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        
         $schedule->call('App\Http\Controllers\Admin\SystemController@update_place_order_status')->everyMinute();
     }
 
