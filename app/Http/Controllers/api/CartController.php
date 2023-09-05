01d8d160b037cee3970966c4a73f1291e38becdb
Amaan <amaan@inoidsolutions.com>
2023-09-05 12:36:33 +0530

    cart empty bug fixes

--- app/Http/Controllers/api/CartController.php
+++ app/Http/Controllers/api/CartController.php
@@ -99,7 +99,7 @@ class CartController extends Controller
             Common::addLog([]);
             return response()->json(['status' => 200, 'message' => 'Success', 'total_price' => number_format($total_cart_price, 2), 'data' => $cart], 200);
         } else {
-            return response()->json(['status' => 200, 'message' => 'Cart is Empty.', 'total_price'=>0,'data'=>[]], 200);
+            return response()->json(['status' => 200, 'message' => 'Cart is Empty.', 'total_price'=> '0','data'=>[]], 200);
         }
     }
 
