f5517cde99377de7db30a6f5d46d0032050d9f54
Amaan <amaan@inoidsolutions.com>
2023-09-08 18:30:10 +0530

    page added

--- resources/views/keepr_app/privacy_policy.blade.php
+++ resources/views/keepr_app/privacy_policy.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Keepr App</title>
+    <title>Privacy and Policy | Keepr App</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('keepr_app_assets/style.css') }}">
     <link rel="icon" type="image/x-icon" href="{{asset("public/company/keepr-favicon.png")}}">
@@ -51,7 +51,7 @@
     <div class="container-fluid" style="padding-top: 80px;">
         <div class="container-fluid" id="Terms_And_Condition">
             <div class="container" id="Terms_And_Condition_main">
-                <h2 class="Heading_terms" style="text-align: center;">TERMS AND CONDITIONS</h2>
+                <h2 class="Heading_terms" style="text-align: center;">PRIVACY AND POLICY</h2>
                 {!! $data ?? "" !!}
                 <!-- <ol>
                     <li><strong>Introduction</strong></li>
